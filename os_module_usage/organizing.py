504a458a26adbada8aee4fea016e08a9e1efe988
Dipan Timalsina <dipantimalsina055@gmail.com>
2025-09-22 20:17:34 +0545

    OS module file organizer tried

--- os_module_usage/organizing.py
+++ os_module_usage/organizing.py
@@ -1,13 +1,25 @@
+#This program is written to demonstrate the use of the os module in Python for organizing files in a directory.
+'''The main function of this program is to organize the pictures in a specific directory based on their file extensions.'''
+
 import os
-import shutil
 
-base_file = "D:\\Python-Programs\\Python-Programs\\os_module_usage"
-image_folder = os.path.join(base_file, "Images")
+image_folder = "D:\Python-Programs\Python-Programs\os_module_usage\image_folder"  # <-- Set your folder path here
 
-if not os.path.exists(image_folder):
-    os.makedirs(image_folder)
+file_types = {
+    'Images': ['.jpg', '.jpeg', '.png', '.webp']
+    # Add more file types and their corresponding extensions as needed
+}
 
-for file_names in os.listdir(base_file):
-    if file_names.endswith((".png", ".jpg", ".jpeg")):
-        shutil.move(os.path.join(base_file, file_names), image_folder)
+organized_files = {key: [] for key in file_types}
 
+for item in os.listdir(image_folder):
+    full_path = os.path.join(image_folder, item)
+    if os.path.isfile(full_path):
+        _, ext = os.path.splitext(item)
+        ext = ext.lower()
+        for type_name, extensions in file_types.items():
+            if ext in extensions:
+                organized_files[type_name].append(item)
+
+for type_name, files in organized_files.items():
+    print(f"{type_name}: {len(files)} files")
