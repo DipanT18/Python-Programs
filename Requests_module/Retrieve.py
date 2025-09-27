6f73ec4e58053ec53a1c7df9bc584cbcde279658
Dipan Timalsina <dipantimalsina055@gmail.com>
2025-09-27 21:26:15 +0545

    Retrieve.py added in Requests_module folder

---
+++ Requests_module/Retrieve.py
@@ -0,0 +1,12 @@
+#This program is written to demonstrate the use of request module to send HTTP GET request to retrieve data 
+
+import requests
+
+response = requests.get('https://www.google.com')
+
+if 'application/json' in response.headers.get('content-type', ''):
+    print(response.json())
+else:
+    print("Response is not JSON. Content type:", response.headers.get('content-type'))
+    print("First 500 characters:")
+    print(response.text[:500])
